10c27ca06b4ca8930c48d820ad07495149b6226c
Miguel del Pino <migueldelpino1996@gmail.com>
2020-03-16 18:59:40 +0100

    Ragdoll y clase demonio base
    Ragdoll simple de greybox como prefab y clase padre de todos los enemigos que pueden ser poseídos

---
+++ Hellevator/Assets/GameAssets/Scripts/ParentClasses/Characters/DemonBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//parent class of demons/enemies that can be controlled by the player
+public class DemonBase : MonoBehaviour
+{
+    private bool isRagdollActive;
+    private bool isControlledByPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
