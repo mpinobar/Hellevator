d284e8d2d6d3cd9ed79f11b0fa2c1a702053342d
clara-algara <33344896+clara-algara@users.noreply.github.com>
2020-03-22 16:32:42 +0100

    Cameras

--- Hellevator/Assets/GameAssets/Scripts/Managers/CameraManager.cs
+++ Hellevator/Assets/GameAssets/Scripts/Managers/CameraManager.cs
@@ -5,11 +5,47 @@ using Cinemachine;
 
 public class CameraManager : TemporalSingleton<CameraManager>
 {
-    [SerializeField] CinemachineVirtualCamera vcam;
+    [SerializeField] CinemachineVirtualCamera vcam1;
+    [SerializeField] CinemachineVirtualCamera vcam2;
+    [SerializeField] GameObject camera1;
+    [SerializeField] GameObject camera2;
+
+    bool isCam1;
+
+
+    private void Start()
+    {
+        camera1.SetActive(true);
+        camera2.SetActive(false);
+
+        isCam1 = true;
+
+        vcam1.Follow = PosesionManager.Instance.ControlledDemon.transform;
+        vcam1.LookAt = PosesionManager.Instance.ControlledDemon.transform;
+    }
 
     public void ChangeCamTarget()
     {
-        vcam.Follow = PosesionManager.Instance.ControlledDemon.transform;
-        vcam.LookAt = PosesionManager.Instance.ControlledDemon.transform;
+        if (isCam1)
+        {
+            vcam2.Follow = PosesionManager.Instance.ControlledDemon.transform;
+            vcam2.LookAt = PosesionManager.Instance.ControlledDemon.transform;
+
+            camera1.SetActive(false);
+            camera2.SetActive(true);
+
+            isCam1 = false;
+        }
+
+        else
+        {
+            vcam1.Follow = PosesionManager.Instance.ControlledDemon.transform;
+            vcam1.LookAt = PosesionManager.Instance.ControlledDemon.transform;
+
+            camera1.SetActive(true);
+            camera2.SetActive(false);
+
+            isCam1 = true;
+        }      
     }
 }
