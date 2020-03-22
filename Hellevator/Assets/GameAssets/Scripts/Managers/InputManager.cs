fff397d4b331b010498293f2b57440333f2b15d6
Adayer <andres.perez@live.u-tad.com>
2020-03-22 17:16:17 +0100

    New Input System

--- Hellevator/Assets/GameAssets/Scripts/Managers/InputManager.cs
+++ Hellevator/Assets/GameAssets/Scripts/Managers/InputManager.cs
@@ -7,7 +7,7 @@ public class InputManager : PersistentSingleton<InputManager>
 {
 
 	Controls m_controls = null;
-    DemonBase currentDemon;
+    DemonBase m_currentDemon;
 	float m_moveInputValue = 0f;
     bool m_jumped;
 
@@ -20,29 +20,20 @@ public class InputManager : PersistentSingleton<InputManager>
 		m_controls = new Controls();
 		m_controls.PlayerControls.InputMove.performed += ctx => m_moveInputValue = ctx.ReadValue<float>(); ;
 		m_controls.PlayerControls.InputMove.canceled += ctx => m_moveInputValue = ctx.ReadValue<float>(); ;
-        m_controls.PlayerControls.InputJump.performed += ctx => m_jumped = true;
-        m_controls.PlayerControls.InputJump.canceled += ctx => m_jumped = false;
-        //m_controls.PlayerControls.InputAbility.performed += ctx =>;
+        m_controls.PlayerControls.InputJump.performed += ctx => Jump();
+        m_controls.PlayerControls.InputAbility.performed += ctx => UseSkill();
         //m_controls.PlayerControls.InputInteract.performed += ctx =>;
-        //m_controls.PlayerControls.InputSuicide.performed += ctx =>;
+        m_controls.PlayerControls.InputSuicide.performed += ctx => PossesNearestDemon();
         UpdateDemonReference();
 	}
 
     // Update is called once per frame
     void Update()
     {
-        if (currentDemon != null)
+        if (m_currentDemon != null)
         {
-            if (Input.GetKeyDown(KeyCode.P))
-            {
-                PosesionManager.Instance.PossessNearestDemon(100, currentDemon);
-            }
-            currentDemon.Move(m_moveInputValue);
-            currentDemon.ToggleWalkingParticles(m_moveInputValue != 0);
-            if (m_jumped)
-            {
-                currentDemon.Jump();
-            }
+            m_currentDemon.Move(m_moveInputValue);
+            m_currentDemon.ToggleWalkingParticles(m_moveInputValue != 0);
         }
         else
         {
@@ -50,9 +41,25 @@ public class InputManager : PersistentSingleton<InputManager>
         }
     }
 
+	void Jump()
+	{
+
+		m_currentDemon.Jump();
+	}
+
+	void PossesNearestDemon()
+	{
+		PosesionManager.Instance.PossessNearestDemon(100, m_currentDemon);
+	}
+
+	void UseSkill()
+	{
+		m_currentDemon.UseSkill();
+	}
+
     public void UpdateDemonReference()
     {
-        currentDemon = PosesionManager.Instance.ControlledDemon;
+        m_currentDemon = PosesionManager.Instance.ControlledDemon;
     }
 
     /*
