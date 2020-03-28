683752e8b9a1a433c413c3e4ffd32784c08c48fc
Albertomz <37207822+Albertomz99@users.noreply.github.com>
2020-03-28 16:38:31 +0100

    Camera Transitions Added

--- Hellevator/Assets/GameAssets/Scripts/Camera/CameraChangeOnTrigger.cs
+++ Hellevator/Assets/GameAssets/Scripts/Camera/CameraChangeOnTrigger.cs
@@ -6,20 +6,36 @@ using Cinemachine;
 public class CameraChangeOnTrigger : MonoBehaviour
 {
     [SerializeField] CameraManager cameraManager;
+    Animator cam1, cam2;
 
-    private void OnTriggerStay2D(Collider2D collision)
+    void Awake()
+    {
+        cam1 = cameraManager.Camera1.GetComponent<Animator>();
+        cam2 = cameraManager.Camera2.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         print("dentroTrigger");
         if (collision.transform.root.GetComponent<DemonBase>() == PosesionManager.Instance.ControlledDemon)
         {
             print(collision.transform.root.GetComponent<DemonBase>());
 
-            cameraManager.IsTriggerFarCamera = true;
+            if(cameraManager.Camera1.activeSelf == true)
+            {
+                cam1.SetTrigger("zoom");
+            }
+            else if(cameraManager.Camera2.activeSelf == true)
+            {
+                cam2.SetTrigger("zoom");
+            }
 
-            cameraManager.Camera1.SetActive(false);
-            cameraManager.Camera2.SetActive(false);
+            //cameraManager.IsTriggerFarCamera = true;
 
-            cameraManager.ChangeCamTarget();
+            //cameraManager.Camera1.SetActive(false);
+            //cameraManager.Camera2.SetActive(false);
+
+            //cameraManager.ChangeCamTarget();
         }            
     }
 
@@ -29,12 +45,21 @@ public class CameraChangeOnTrigger : MonoBehaviour
         {
             print("FueraTrigger");
 
-            cameraManager.IsTriggerFarCamera = false;
+            if (cameraManager.Camera1.activeSelf == true)
+            {
+                cam1.SetTrigger("zoom");
+            }
+            else if (cameraManager.Camera2.activeSelf == true)
+            {
+                cam2.SetTrigger("zoom");
+            }
 
-            cameraManager.Camera3.SetActive(false);
-            cameraManager.Camera4.SetActive(false);
+            //cameraManager.IsTriggerFarCamera = false;
 
-            cameraManager.ChangeCamTarget();
+            //cameraManager.Camera3.SetActive(false);
+            //cameraManager.Camera4.SetActive(false);
+
+            //cameraManager.ChangeCamTarget();
         }
     }
 }
