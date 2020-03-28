ab7ae8d19388dad33344756a41f8913cef6c7ba8
clara-algara <33344896+clara-algara@users.noreply.github.com>
2020-03-28 11:51:06 +0100

    Camerav1

---
+++ Hellevator/Assets/GameAssets/Scripts/Camera/CameraChangeOnTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraChangeOnTrigger : MonoBehaviour
+{
+    [SerializeField] CameraManager cameraManager;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        cameraManager.IsOnTriggerPz = true;
+
+        cameraManager.Camera1.SetActive(false);
+        cameraManager.Camera2.SetActive(false);
+
+        cameraManager.ChangeCamTarget();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        cameraManager.IsOnTriggerPz = false;
+
+        cameraManager.Camera3.SetActive(false);
+        cameraManager.Camera4.SetActive(false);
+
+        cameraManager.ChangeCamTarget();
+    }
+}
