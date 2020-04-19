7f26f432fc70de7e8e00c186c4ab7223914ee770
Miguel del Pino <migueldelpino1996@gmail.com>
2020-04-19 13:42:57 +0200

    Palo gordo ahora mueve a los cuerpos consigo

--- Hellevator/Assets/GameAssets/Scripts/LevelElements/HorizontalPeriodicPlatform.cs
+++ Hellevator/Assets/GameAssets/Scripts/LevelElements/HorizontalPeriodicPlatform.cs
@@ -12,9 +12,17 @@ public class HorizontalPeriodicPlatform : MonoBehaviour
     private Vector3 m_endPos;
     private bool m_returningToInitialPosition;
     private float m_waitTimer;
+    private Vector3 m_lastPosition;
+    private float m_lastMovedHorizontalDelta;
+
+    private List<DemonBase> m_enemiesOnPreassurePlate;
+    [SerializeField] private LayerMask m_enemyLayerMask;
+    List<SpikesWeightData> m_spikesData;
 
     private void Awake()
     {
+        m_enemiesOnPreassurePlate = new List<DemonBase>();
+        m_spikesData = new List<SpikesWeightData>();
         m_endPos = m_endPosition.position;
         m_waitTimer = m_waitTimeOnArrival;
         m_initialPosition = transform.position;
@@ -53,4 +61,75 @@ public class HorizontalPeriodicPlatform : MonoBehaviour
             }
         } 
     }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       
+        DemonBase cmpDemon = collision.transform.GetComponentInParent<DemonBase>();
+
+        if (cmpDemon != null)
+        {
+            bool isCounted = false;
+
+            for (int i = 0; i < m_spikesData.Count; i++)
+            {
+                //if the demon is already inside the spikes
+                if (cmpDemon == m_spikesData[i].AssociatedDemon)
+                {
+                    isCounted = true;
+
+                    //add the collider to the associated demon's collider list if it isnt already included
+                    if (!m_spikesData[i].Colliders.Contains(collision.collider) && collision.gameObject.tag != "BodyCollider")
+                    {
+                        m_spikesData[i].Colliders.Add(collision.collider);
+                    }
+                }
+            }
+            if (!isCounted)
+            {
+
+                m_spikesData.Add(new SpikesWeightData(cmpDemon, collision.collider));
+                m_enemiesOnPreassurePlate.Add(cmpDemon);
+                cmpDemon.transform.parent = transform;
+
+            }
+
+        }
+    }
+    
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        DemonBase cmpDemon = collision.transform.GetComponentInParent<DemonBase>();
+        if (cmpDemon != null && collision.gameObject.tag != "BodyCollider")
+        {
+
+            for (int i = 0; i < m_spikesData.Count; i++)
+            {
+                //if the demon is already inside the spikes
+                if (cmpDemon == m_spikesData[i].AssociatedDemon)
+                {
+                    //remove the collider from the associated demon's collider list 
+                    if (m_spikesData[i].Colliders.Contains(collision.collider))
+                    {
+                        m_spikesData[i].Colliders.Remove(collision.collider);
+
+                        //all the limbs have exited the spikes
+                        if (m_spikesData[i].Colliders.Count == 0)
+                        {                            
+                            m_spikesData.RemoveAt(i);
+                            m_enemiesOnPreassurePlate.Remove(cmpDemon);
+                            cmpDemon.transform.parent = null;
+                        }
+                        else if (m_spikesData[i].Colliders.Count == 1 && m_spikesData[i].Colliders[0].tag == "BodyCollider")
+                        {
+                            m_enemiesOnPreassurePlate.Remove(cmpDemon);                            
+                            m_spikesData.RemoveAt(i);
+                            cmpDemon.transform.parent = null;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
 }
