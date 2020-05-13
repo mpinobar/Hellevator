5f15ec29cc243f7a9b147336e8e2743034a24f34
Miguel del Pino <migueldelpino1996@gmail.com>
2020-05-13 10:45:50 +0200

    Funciona?

---
+++ Hellevator/Assets/GameAssets/Scripts/DataClasses/HorizontalTransportData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalTransportData
+{
+    private Rigidbody2D m_demonRgb;
+    private bool m_isVelocityApplied;
+
+    public Rigidbody2D DemonRgb { get => m_demonRgb; set => m_demonRgb = value; }
+    public bool IsVelocityApplied { get => m_isVelocityApplied; set => m_isVelocityApplied = value; }
+
+    public HorizontalTransportData(Rigidbody2D demon, bool isApplied)
+    {
+        m_demonRgb = demon;
+        m_isVelocityApplied = isApplied;
+    }
+}
---
+++ Hellevator/Assets/GameAssets/Scripts/LevelElements/HorizontalTransport.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalTransport : MonoBehaviour
+{
+    [SerializeField] private float m_speed = 2f;
+    [SerializeField] private TransportDirection direction = TransportDirection.Right;
+    public List<DemonBase> m_enemiesOnPreassurePlate;
+    [SerializeField] private LayerMask m_enemyLayerMask;
+    List<SpikesWeightData> m_spikesData;
+    List<HorizontalTransportData> m_charactersRgbs;
+    private float dir;
+    private List<Transform> m_bonesTransforms;
+    private List<Vector3> m_bonesPositions;
+    private List<Quaternion> m_bonesRotations;
+    private void Awake()
+    {
+        m_enemiesOnPreassurePlate = new List<DemonBase>();
+        m_spikesData = new List<SpikesWeightData>();
+        m_charactersRgbs = new List<HorizontalTransportData>();
+        dir = (int) direction * 2 - 1;
+        m_bonesTransforms = new List<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < m_enemiesOnPreassurePlate.Count; i++)
+        {
+            m_enemiesOnPreassurePlate[i].DragMovement(dir*m_speed);
+        }
+
+
+        for (int i = 0; i < m_spikesData.Count; i++)
+        {
+            
+        }
+
+    }
+
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        DemonBase cmpDemon = collision.transform.GetComponentInParent<DemonBase>();
+
+        if (cmpDemon != null)
+        {
+            bool isCounted = false;
+
+            for (int i = 0; i < m_spikesData.Count; i++)
+            {
+                //if the demon is already inside the spikes
+                if (cmpDemon == m_spikesData[i].AssociatedDemon)
+                {
+                    isCounted = true;
+
+                    //add the collider to the associated demon's collider list if it isnt already included
+                    if (!m_spikesData[i].Colliders.Contains(collision.collider) && collision.gameObject.tag != "BodyCollider")
+                    {
+                        m_spikesData[i].Colliders.Add(collision.collider);
+                        
+                    }
+                }
+            }
+            if (!isCounted)
+            {
+                m_spikesData.Add(new SpikesWeightData(cmpDemon, collision.collider));
+                m_charactersRgbs.Add(new HorizontalTransportData(cmpDemon.Torso.GetComponent<Rigidbody2D>(),false));
+                cmpDemon.DragMovement(m_speed * dir);
+                m_enemiesOnPreassurePlate.Add(cmpDemon);
+                
+
+            }
+
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        DemonBase cmpDemon = collision.transform.GetComponentInParent<DemonBase>();
+        if (cmpDemon != null && collision.gameObject.tag != "BodyCollider")
+        {
+
+            for (int i = 0; i < m_spikesData.Count; i++)
+            {
+                //if the demon is already inside the spikes
+                if (cmpDemon == m_spikesData[i].AssociatedDemon)
+                {
+                    //remove the collider from the associated demon's collider list 
+                    if (m_spikesData[i].Colliders.Contains(collision.collider))
+                    {
+                        m_spikesData[i].Colliders.Remove(collision.collider);
+
+                        //all the limbs have exited the spikes
+                        if (m_spikesData[i].Colliders.Count == 0)
+                        {
+                            cmpDemon.DragMovement(0);
+                            m_spikesData.RemoveAt(i);
+                            m_enemiesOnPreassurePlate.Remove(cmpDemon);
+                        }
+                        else if (m_spikesData[i].Colliders.Count == 1 && m_spikesData[i].Colliders[0].tag == "BodyCollider")
+                        {
+                            cmpDemon.DragMovement(0);
+                            m_enemiesOnPreassurePlate.Remove(cmpDemon);
+                            m_spikesData.RemoveAt(i);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+}
