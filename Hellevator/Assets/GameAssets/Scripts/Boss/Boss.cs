e62f580ecee1d00d3fe8b45fe4c04f9a899af5ae
mpinobar <migueldelpino1996@gmail.com>
2020-10-19 22:53:56 +0200

    Inicio de boss y discoteca

---
+++ Hellevator/Assets/GameAssets/Scripts/Boss/Boss.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : TemporalSingleton<Boss>
+{
+
+    private enum State
+    {
+        Default, SeeingPlayer
+    }
+
+    [SerializeField] GameObject m_doorToCloseUponStart;
+    [SerializeField] float m_timeUntilPlayerDeath;
+    float m_playerSeenDeathTimer = 0f;
+
+    State m_currentState = State.Default;
+
+    public override void Awake()
+    {
+        base.Awake();
+        m_playerSeenDeathTimer = 0f;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_currentState == State.SeeingPlayer)
+        {
+            m_playerSeenDeathTimer += Time.deltaTime;
+            if(m_playerSeenDeathTimer >= m_timeUntilPlayerDeath)
+            {
+                KillPlayer();
+            }
+        }        
+    }
+
+    public void SetSeeingPlayer()
+    {
+        m_currentState = State.SeeingPlayer;
+    }
+
+    public void SetNotSeeingPlayer()
+    {
+        m_currentState = State.Default;
+        m_playerSeenDeathTimer = 0f;
+    }
+
+    private void KillPlayer()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CloseEntrance()
+    {
+        //m_doorToCloseUponStart.Close();
+    }
+}
--- Hellevator/Assets/GameAssets/Scripts/UI/MainMenu/MainMenuCanvasController.cs
+++ Hellevator/Assets/GameAssets/Scripts/UI/MainMenu/MainMenuCanvasController.cs
@@ -21,8 +21,13 @@ public class MainMenuCanvasController : MonoBehaviour
     {
         m_tmp = m_fadeTime * 0.5f;
         m_fadingIn = false;
+<<<<<<< Updated upstream
         ChangeState(MenuCameraState.Default);
         SetMenuActive();
+=======
+        ChangeState(MenuCameraState.Options);
+        SetMenuActive();        
+>>>>>>> Stashed changes
     }
 
 
