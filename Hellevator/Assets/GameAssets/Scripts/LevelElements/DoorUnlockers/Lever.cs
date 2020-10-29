161acdc4cbe7f82ec55dcbac7ea9481d34e9626c
mpinobar <migueldelpino1996@gmail.com>
2020-10-29 14:23:38 +0100

    palanca script

---
+++ Hellevator/Assets/GameAssets/Scripts/LevelElements/DoorUnlockers/Lever.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : ButtonActivatedBase
+{
+    [SerializeField] ButtonActivatedDoor m_doorToUnlock;
+
+    public override void Activate()
+    {
+        m_doorToUnlock.Activate();
+    }
+        
+}
