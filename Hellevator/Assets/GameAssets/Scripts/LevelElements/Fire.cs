28e00006dd29b1b0cbfecd1ccc6bb990d9d2b169
mpinobar <migueldelpino1996@gmail.com>
2020-11-13 15:22:14 +0100

    Fuego ahora mata a personajes, fix de shaders

--- Hellevator/Assets/GameAssets/Scripts/LevelElements/Fire.cs
+++ Hellevator/Assets/GameAssets/Scripts/LevelElements/Fire.cs
@@ -10,6 +10,10 @@ public class Fire : MonoBehaviour
     SpriteRenderer m_spriteRenderer;
     MaterialPropertyBlock m_propertyBlock;
     float m_currentFireAltitude = 1;
+    Vector2 m_size;
+    [SerializeField] LayerMask m_detectionLayer;
+    RaycastHit2D[] m_impacts;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,25 +23,47 @@ public class Fire : MonoBehaviour
         m_spriteRenderer = GetComponent<SpriteRenderer>();
         m_propertyBlock = new MaterialPropertyBlock();
         m_spriteRenderer.GetPropertyBlock(m_propertyBlock);
+        m_size = new Vector2(transform.localScale.y * 0.5f, 1);
     }
-    //RaycastHit2D raycastHit;
+
     // Update is called once per frame
     void Update()
     {
         Debug.DrawRay(m_startingPoint, (m_endPoint - m_startingPoint) * m_currentFireAltitude, Color.green);
-        RaycastHit2D raycastHit = Physics2D.Raycast(m_startingPoint, m_endPoint - m_startingPoint, m_height * m_currentFireAltitude);
-        if (raycastHit.transform != null)
+        Debug.DrawRay(m_startingPoint - (m_size * 0.5f) * Vector2.right, (m_endPoint - m_startingPoint) * m_currentFireAltitude, Color.green);
+        Debug.DrawRay(m_startingPoint + (m_size * 0.5f) * Vector2.right, (m_endPoint - m_startingPoint) * m_currentFireAltitude, Color.green);
+        
+        m_impacts = Physics2D.BoxCastAll(m_startingPoint,m_size, 0, m_endPoint - m_startingPoint, m_height * m_currentFireAltitude, m_detectionLayer);
+        if(m_impacts.Length > 0)
         {
-            //Debug.LogError(raycastHit.transform.name);
-            //m_spriteRenderer.GetPropertyBlock(m_propertyBlock);
-            float distance = Vector2.Distance(raycastHit.transform.position, m_startingPoint);
-            m_currentFireAltitude = distance / m_height;
-            m_propertyBlock.SetFloat("_height", Mathf.Clamp01(m_currentFireAltitude));
-            m_spriteRenderer.SetPropertyBlock(m_propertyBlock);
-        }
+            for (int i = 0; i < m_impacts.Length; i++)
+            {
+                if (m_impacts[i].transform != null)
+                {
+                    if (LayerMask.LayerToName(m_impacts[i].transform.gameObject.layer) == "Player")
+                    {
+                        m_impacts[i].transform.GetComponent<DemonBase>().Die(false);
+                        Destroy(m_impacts[i].transform.gameObject);
+                    }
+                    else if (LayerMask.LayerToName(m_impacts[i].transform.gameObject.layer) == "Ragdoll")
+                    {
+                        if (m_impacts[i].transform.GetComponentInParent<DemonBase>())
+                        {
+                            Destroy(m_impacts[i].transform.GetComponentInParent<DemonBase>().gameObject);
+                        }                        
+                    }
+                    else
+                    {
+                        m_currentFireAltitude = Mathf.Clamp01(m_impacts[i].distance / m_height);
+                        m_propertyBlock.SetFloat("_height", Mathf.Clamp01(m_currentFireAltitude));
+                        m_spriteRenderer.SetPropertyBlock(m_propertyBlock);
+                    }                    
+                }
+            }           
+        }        
         else
         {
-            m_currentFireAltitude = Mathf.Lerp(m_currentFireAltitude, 1, Time.deltaTime);
+            m_currentFireAltitude = Mathf.Clamp01(Mathf.Lerp(m_currentFireAltitude, 1, Time.deltaTime));
             m_propertyBlock.SetFloat("_height", Mathf.Clamp01(m_currentFireAltitude));
             m_spriteRenderer.SetPropertyBlock(m_propertyBlock);
         }
