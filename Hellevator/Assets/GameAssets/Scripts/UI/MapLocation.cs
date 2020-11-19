bfaf0b17294b7825792bd186ac000412fc5c52eb
mpinobar <migueldelpino1996@gmail.com>
2020-11-19 16:25:56 +0100

    Prototipo de mapa

---
+++ Hellevator/Assets/GameAssets/Scripts/UI/MapLocation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class MapLocation : MonoBehaviour
+{
+    [SerializeField] string m_location;
+    Image m_locationImage;
+
+    private void Awake()
+    {
+        m_locationImage = GetComponent<Image>();
+    }
+
+    public string Location { get => m_location; set => m_location = value; }
+
+    public void ActivateLocation()
+    {
+        m_locationImage.color = Color.green;
+    }
+    public void DeactivateLocation()
+    {
+        m_locationImage.color = Color.white;
+    }
+
+}
