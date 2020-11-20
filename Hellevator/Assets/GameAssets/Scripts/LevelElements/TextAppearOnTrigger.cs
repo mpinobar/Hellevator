b0fada1c620bef2d284f25963180b4da4054efb4
Adayer <andres.perez@live.u-tad.com>
2020-11-20 13:05:33 +0100

    Cambios en R.5,7,9,11,15

---
+++ Hellevator/Assets/GameAssets/Scripts/LevelElements/TextAppearOnTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextAppearOnTrigger : MonoBehaviour
+{
+	[SerializeField] GameObject m_objectToActivate = null;
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.GetComponent<DemonBase>() == PossessionManager.Instance.ControlledDemon)
+		{
+			m_objectToActivate.SetActive(true);
+		}
+	}
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.GetComponent<DemonBase>() == PossessionManager.Instance.ControlledDemon)
+		{
+			m_objectToActivate.SetActive(false);
+		}
+	}
+}
