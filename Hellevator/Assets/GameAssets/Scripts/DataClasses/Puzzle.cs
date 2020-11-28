d7ce01d43555c887e9fedb9b4ee7e5f8d9a77113
mpinobar <migueldelpino1996@gmail.com>
2020-11-28 18:19:02 +0100

    introducido el menu de coleccionables y fixes menores

--- Hellevator/Assets/GameAssets/Scripts/DataClasses/Puzzle.cs
+++ Hellevator/Assets/GameAssets/Scripts/DataClasses/Puzzle.cs
@@ -32,27 +32,21 @@ public class Puzzle : MonoBehaviour
 
     public void TrySetPuzzleSolved()
     {
-        //Debug.LogError("Part of a puzzle was solved. Checking if all are");
         for (int i = 0; i < m_completeWhenActive.Length; i++)
         {
             if (!m_completeWhenActive[i].Active)
             {
-                //Debug.LogError("Puzzle with id " + m_ID + " was NOT solved");
                 return;
             }
         }
-
-        //Debug.LogError("Puzzle with id " + m_ID + " was solved. Saving puzzle completion");
         PuzzlesDataManager.SetPuzzleSolved(m_ID);
 
     }
 
     public void CheckSolvePuzzle()
     {
-        //Debug.LogError("Checking if puzzle with id " + m_ID + " was solved");
         if (PuzzlesDataManager.CheckPuzzle(m_ID))
         {
-            Debug.LogError("Puzzle was solved. Unlocking all puzzle parts");
             for (int i = 0; i < m_completeWhenActive.Length; i++)
             {
                 m_completeWhenActive[i].ActivateImmediately();
@@ -64,10 +58,6 @@ public class Puzzle : MonoBehaviour
                     m_secondaryActivations[i].ActivateImmediately();
                 }
             }
-        }
-        else
-        {
-            //Debug.LogError("Puzzle with id " + m_ID + " was NOT solved");
         }
     }
 
