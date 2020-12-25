dafc2fb49dee9daf5491a99a5b693cbb02569390
mpinobar <migueldelpino1996@gmail.com>
2020-12-25 13:54:23 +0100

    TIMMY IS BACK BITCHES Luces en R.4 y llama de vela.

---
+++ Hellevator/Assets/GameAssets/Scripts/Effects/Light2DSpriteSwap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class Light2DSpriteSwap : MonoBehaviour
+{
+    //[SerializeField] Sprite sp;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    Light2D light = GetComponent<Light2D>();
+    //    light.lightCookieSprite = 
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
