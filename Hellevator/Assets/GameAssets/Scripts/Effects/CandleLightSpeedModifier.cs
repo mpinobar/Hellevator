1c909f7a1fe09f262aebc38dd41ff16c88c64fbc
mpinobar <migueldelpino1996@gmail.com>
2020-12-25 17:53:59 +0100

    Control de velocidad de las llamas

---
+++ Hellevator/Assets/GameAssets/Scripts/Effects/CandleLightSpeedModifier.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleLightSpeedModifier : MonoBehaviour
+{
+    [SerializeField] float m_speed;
+
+    private void Start()
+    {
+        transform.GetChild(0).GetComponent<Animator>().speed = m_speed;
+        transform.GetChild(0).GetChild(0).GetComponent<Animator>().speed = m_speed;
+    }
+}
