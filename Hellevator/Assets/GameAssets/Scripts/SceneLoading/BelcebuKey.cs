a3d419cccd5e94f9bc21f16c8ebf0b7388b0d682
Adayer <andres.perez@live.u-tad.com>
2021-05-02 20:08:04 +0200

    Cambio de escena bossroom -> r.0 -> j.1 funcional

---
+++ Hellevator/Assets/GameAssets/Scripts/SceneLoading/BelcebuKey.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BelcebuKey : MonoBehaviour
+{
+    [SerializeField] private string m_linkedScene;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.TryGetComponent<DemonBase>(out DemonBase demon))
+        {
+            if(demon == PossessionManager.Instance.ControlledDemon)
+            {
+                PossessionManager.Instance.ChangeMainCharacter(PossessionManager.Instance.ControlledDemon);
+                LevelManager.Instance.SwitchToAdjacentScene(m_linkedScene);
+                InputManager.Instance.IsInInteactionTrigger = false;
+                InputManager.Instance.IsInDialogue = false;
+            }
+        }
+    }
+}
