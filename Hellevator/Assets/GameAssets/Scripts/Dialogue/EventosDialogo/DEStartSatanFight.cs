58357911824f7a78ebea36052b819c98b166dab9
Adayer <andres.perez@live.u-tad.com>
2021-05-09 12:36:17 +0200

    Chatty chat con botones pre fight

---
+++ Hellevator/Assets/GameAssets/Scripts/Dialogue/EventosDialogo/DEStartSatanFight.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEStartSatanFight : DialogEvent
+{
+    [SerializeField] Satan m_satan;
+    [SerializeField] GameObject m_botones;
+
+    public override void ActivateEvent()
+    {
+        m_satan.BeginFight();
+        m_botones.SetActive(false);
+    }
+}
