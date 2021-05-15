04709029044f92e3f1f6862b2433a7360b4f3922
Adayer <andres.perez@live.u-tad.com>
2021-05-15 18:32:41 +0200

    Casi cutscene final

---
+++ Hellevator/Assets/GameAssets/Scripts/Dialogue/EventosDialogo/DEAfterFinalConversation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class DEAfterFinalConversation : DialogEvent
+{
+    [SerializeField] private CinemachineVirtualCamera m_camera;
+    [SerializeField] private DestructibleWall m_trapDoor;
+    [SerializeField] private Transform m_fall;
+    [SerializeField] private Transform m_auxTrans;
+
+    public override void ActivateEvent()
+    {
+        m_fall.SetParent(m_auxTrans, true);
+        m_camera.Priority = 2;
+        m_trapDoor.Explode(PossessionManager.Instance.ControlledDemon.transform.position, 8);
+        PossessionManager.Instance.ControlledDemon.GetComponent<Animator>().SetTrigger("Fall");
+
+    }
+}
