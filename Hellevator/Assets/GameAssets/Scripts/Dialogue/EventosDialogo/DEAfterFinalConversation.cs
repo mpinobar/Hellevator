3a9f22e6c68930322b03214b08cd98f2fa503656
mpinobar <migueldelpino1996@gmail.com>
2021-05-16 20:58:09 +0200

    sonorizadas conversaciones finales y fix de stutter de camara en ascensor

--- Hellevator/Assets/GameAssets/Scripts/Dialogue/EventosDialogo/DEAfterFinalConversation.cs
+++ Hellevator/Assets/GameAssets/Scripts/Dialogue/EventosDialogo/DEAfterFinalConversation.cs
@@ -9,13 +9,13 @@ public class DEAfterFinalConversation : DialogEvent
     [SerializeField] private DestructibleWall m_trapDoor;
     [SerializeField] private Transform m_fall;
     [SerializeField] private Transform m_auxTrans;
-
+    [SerializeField] RuntimeAnimatorController animatorController;
     public override void ActivateEvent()
     {
         m_fall.SetParent(m_auxTrans, true);
         m_camera.Priority = 2;
         m_trapDoor.Explode(PossessionManager.Instance.ControlledDemon.transform.position, 80);
-
+        PossessionManager.Instance.ControlledDemon.GetComponent<Animator>().runtimeAnimatorController = animatorController;
         PossessionManager.Instance.ControlledDemon.GetComponent<Animator>().SetBool("falling", true);
         PossessionManager.Instance.ControlledDemon.CanMove = true;
         InputManager.Instance.IsInInteactionTrigger = true;
